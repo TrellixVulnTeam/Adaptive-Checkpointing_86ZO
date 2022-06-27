9603372fb8f4a7ad92653271d0088f7a4e1c08c9
AlbertAn666 <yn_an@outlook.com>
2022-06-26 23:52:28 -0700

    bugfix wrong file name in shell

--- experiment-tools/start-exp.sh
+++ experiment-tools/start-exp.sh
@@ -202,5 +202,5 @@ cd "$FLINKROOT"/experiment-tools/ || (echo "cd fail" && exit 1)
 python3 flink_connector.py --job_id "$QUERY_ID" --interval "$FETCH_INTERVAL" --total_time "$FETCH_TOTAL_TIME"
 
 # clear all jobs and topics
-. "$bin"/clear_prev_jobs.sh
-. "$bin"/clear_kafka_topics.sh
+. "$bin"/clear-prev-jobs.sh
+. "$bin"/clear-kafka-topics.sh
