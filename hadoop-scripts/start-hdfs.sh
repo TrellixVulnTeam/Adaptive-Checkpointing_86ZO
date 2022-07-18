bb17e8cc3b99fecae1f35c4cd30bf01bea8e9695
mingcui <mingcuiyang980407@gmail.com>
2022-07-18 12:36:44 -0400

    updates

--- hadoop-scripts/start-hdfs.sh
+++ hadoop-scripts/start-hdfs.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+export FLINKROOT=$(cd ..; pwd)
+export HADOOP_HOME=/usr/local/hadoop
+export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop
 
 # stop all the nodes
+. "$bin"/clear-prev-jobs.sh
 cd ~
-cd $HADOOP_HOME/sbin
-stop-all.sh
+cd $HADOOP_HOME/sbin || (echo "cd fail" && exit 1)
+sh stop-all.sh
 
 iplist=()
 # get workers
-cd "$FLINKROOT"/hadoop-scripts/
+cd "$FLINKROOT"/hadoop-scripts/ || (echo "cd fail" && exit 1)
 while IFS= read -r line; do
   ip="$line"
   printf '%s\n' $ip
@@ -32,5 +36,5 @@ done
 
 # start-all
 cd ~
-cd $HADOOP_HOME/sbin
-start-all.sh
+cd $HADOOP_HOME/sbin || (echo "cd fail" && exit 1)
+sh start-all.sh
