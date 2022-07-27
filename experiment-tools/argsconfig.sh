8bd6875e9d315480c921a136b2d74527c775ee72
AlbertAn666 <yn_an@outlook.com>
2022-07-27 13:16:11 -0700

    hdfs

--- experiment-tools/argsconfig.sh
+++ experiment-tools/argsconfig.sh
@@ -3,7 +3,7 @@ GROUP="adaptive"
 EXCHANGE_RAGE="0.82F"
 HDFS_DIR="hdfs://10.0.0.181:9000/checkpoint"
 MEMORY_DIR="file:///home/ubuntu/Adaptive-Checkpointing-Storage/Checkpoint"
-CHECKPOINT_DIR=$MEMORY_DIR
+CHECKPOINT_DIR=$HDFS_DIR
 INCREMENTAL_CHECKPOINTS="false"
 LATENCY_MARKER_INTERVAL=60000
 
