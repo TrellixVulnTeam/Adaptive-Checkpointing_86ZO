7e2385e4b10863dc7e24355cd44abfe52778773f
mingcui <mingcuiyang980407@gmail.com>
2022-08-01 16:26:21 -0400

    codes for drawing kde and hist of latency

--- experiment-tools/draw_latency_result.py
+++ experiment-tools/draw_latency_result.py
@@ -3,8 +3,8 @@ import sys
 import json
 import pandas
 import numpy as np
-import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def get_data(src_dir):
     if os.path.exists(src_dir + "/latency.json"):
@@ -24,23 +24,36 @@ def remove_error_data(list):
     list = filter(lambda x: x >= list_mean - 3*list_std & x <= list_mean + 3*list, list)
     return list, list_mean, list_std
 
-def draw_result(list, list_mean, list_std, exp_type, save_path):
-    num_bins = 20
-    n, bins, patches = plt.hist(list, num_bins, normed=1, facecolor='blue', alpha=0.8) # 直方图
-    y = mlab.normpdf(bins, list_mean, list_std)  # 拟合概率分布
-    plt.plot(bins, y, 'r--') #绘制y的曲线
-    plt.xlabel('Latency (ms)') #绘制x轴
-    plt.ylabel('Probability') #绘制y轴
-    plt.title('Latency distribution based on ' + exp_type + ' strategy')
-    plt.savefig(save_path)
+def draw_result(src_dir, target_dir):
+    #get data
+    save_hist_path = target_dir + "/latency.jpg"
+    save_kde_path = target_dir + "/kde.jpg"
+    latency_info = get_data(src_dir)
+
+    #draw histograme
+    bins = 20
+    plt.style.use('seaborn-deep')
+    plt.cla()
+    plt.hist(latency_info.values(), bins, label=[l for l in latency_info.keys()])
+    plt.legend(loc = "upper right")
+    plt.xlabel('Latency (ms)',fontsize = 10)
+    plt.ylabel('Frequency', fontsize = 10)
+    plt.title('Latency distribution', fontsize = 10)
+    plt.savefig(save_hist_path)
+    plt.show()
+
+    #draw kde
+    df = pd.DataFrame.from_dict(latency_info)
+    print(df)
+    sns.kdeplot(data=df, clip=(0, 1000))
+    plt.xlabel('Latency (ms)',fontsize = 10)
+    plt.ylabel('Frequency', fontsize = 10)
+    plt.title('Latency distribution', fontsize = 10)
+    plt.savefig(save_kde_path)
     return
 
 def main(src_dir, target_dir):
-    base_data, new_data = get_data(src_dir)
-    base_data, base_mean, base_std = remove_error_data(base_data)
-    draw_result(base_data, base_mean, base_std, "default", target_dir + "/latency_default.jpg")
-    new_data, new_mean, new_std = remove_error_data(new_data)
-    draw_result(new_data, new_mean, new_std, "new", target_dir + "/latency_new.jpg")
+    draw_result(src_dir, target_dir)
     return
 
 if __name__ = "__main__":
