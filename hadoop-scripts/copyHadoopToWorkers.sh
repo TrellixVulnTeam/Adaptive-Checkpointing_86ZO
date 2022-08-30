a0a2571423158cf152366017c6ec7aae122ad7f5
Yang Minghui <minghuiyang1998@gmail.com>
2022-08-29 23:21:29 -0400

    update sh

--- hadoop-scripts/copyHadoopToWorkers.sh
+++ hadoop-scripts/copyHadoopToWorkers.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-localip=$(hostname -I)
 iplist=()
 while IFS= read -r line; do
   ip="$line"
   printf '%s\n' $ip
-  if [ ! -z "$ip" ]; then
+  if [[ (! -z "$ip") && ($ip != "flinknode-1") ]]; then
     iplist+=("$ip")
   fi
 done < workers
 
 for ip in "${iplist[@]}"
 do
-  if [[ $ip != "flinknode-1" ]]; then
-    echo "copy hadoop to $ip"
-    scp -r $HADOOP_HOME $ip:/usr/local/
-  fi
+  echo "copy hadoop to $ip"
+  scp -r $HADOOP_HOME $ip:/usr/local/
 done
