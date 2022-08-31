7815f8f1fd6ea2de5d1b354e085bbc87301beffa
Yang Minghui <minghuiyang1998@gmail.com>
2022-08-31 17:12:40 -0400

    shell script bugfix

--- experiment-tools/clear-prev-jobs.sh
+++ experiment-tools/clear-prev-jobs.sh
@@ -11,8 +11,9 @@ echo "FLINKROOT: $FLINKROOT"
 FLINK_TARGET="$FLINKROOT"/flink-dist/target/flink-1.14.0-bin/flink-1.14.0
 
 rm $STORAGE_FILE
-# 1. read all jobs as list
-"$FLINK_TARGET"/bin/flink list > "$STORAGE_FILE"
+# 1. read all jobs as list, avoid keep waiting when flink is not started
+timeout 4 "$FLINK_TARGET"/bin/flink list & > "$STORAGE_FILE"
+wait $!
 # 2. use a loop to clean every jobs
 joblist=()
 while IFS= read -r line; do
@@ -25,7 +26,7 @@ done < $STORAGE_FILE
 for job in "${joblist[@]}"
 do
   printf 'delete %s\n' $job
-  "$FLINK_TARGET"/bin/flink cancel "$job"
+  "$FLINK_TARGET"/bin/flink cancel "$job" &
 done
 
 # 3. remove alljobs
