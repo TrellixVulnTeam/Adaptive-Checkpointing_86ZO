4866ca7547db18b079a4be2114d2cf8124edf0d5
Yang Minghui <minghuiyang1998@gmail.com>
2022-10-14 14:37:03 -0400

    update kill script

--- experiment-tools/kill-taskmanager.sh
+++ experiment-tools/kill-taskmanager.sh
@@ -2,20 +2,10 @@
 USAGE="Usage: ./kill-taskmanager.sh KILL_TIME &"
 KILL_TIME=$1
 echo "kill the taskmanger after $KILL_TIME second"
-STORAGE_FILE=allprocess
 # clear previous file
-rm $STORAGE_FILE
 sleep "$KILL_TIME"
-jps > $STORAGE_FILE
-
-while IFS= read -r line; do
-  str=$line
-  name="TaskManagerRunner"
-  res=$(echo "$str" | grep "${name}")
-  p=$( grep -o '[0-9]+' <<< "$str" )
-  if [ ! -z "$res" ] && [ ! -z "$p" ] ; then
-    echo "pid is $p"
-    kill -9 "$p"
-    break
-  fi
-done < $STORAGE_FILE
+var=$(jps | grep TaskManagerRunner)
+array=($var)
+pid=${array[0]}
+echo "pid is $pid"
+kill -9 "$pid"
